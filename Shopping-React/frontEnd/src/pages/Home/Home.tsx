8f35dd86b5b9a8f57d9f3cbb0aa2402bec15b79c
ducanhaovai <ducanhaovai@users.noreply.github.com>
2024-03-23 19:17:03 +0700

    Update Search

--- Shopping-React/frontEnd/src/pages/Home/Home.tsx
+++ Shopping-React/frontEnd/src/pages/Home/Home.tsx
@@ -10,48 +10,41 @@ const Home = ({ searchTerm }) => {
   const [products, setProducts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(10);
-  const [searchResults, setSearchResults] = useState([]);
-  const [searching, setSearching] = useState(false);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get("http://localhost:8088/products");
-        setProducts(response.data);
+        if (!searchTerm) {
+          const response = await axios.get("http://localhost:8088/products");
+          setProducts(response.data);
+        }
       } catch (error) {
         console.error("Error fetching products:", error);
       }
     };
 
-    if (!searchTerm) {
-      fetchProducts();
-      setSearching(false);
-    } else {
-      setSearchResults([searchTerm]); // Sử dụng searchTerm trực tiếp trong kết quả tìm kiếm
-      setSearching(true);
-    }
+    fetchProducts();
   }, [searchTerm]);
 
   const handlePagination = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
-  const totalItems = searching ? searchResults.length : products.length;
+  const totalItems = searchTerm ? searchTerm.length : products.length;
 
   return (
     <div>
       <div className="mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-        {(searchTerm && searching ? searchResults : products) // Check if searchTerm exists and searching is true
-          .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
-          .map((product, index) => (
+        {(searchTerm && searchTerm.length > 0 ? searchTerm : products).map(
+          (product, index) => (
             <div className="col-span-1" key={product.id || index}>
               <Link to={`/products/${product.id}`}>
                 <div className="overflow-hidden rounded-sm bg-white shadow transition-transform duration-100 hover:translate-y-[-0.04rem] hover:shadow-md">
                   <div className="relative w-full pt-[100%]">
                     <img
-                      src={product.images}
+                      src={product.images[0]}
                       className="absolute top-0 left-0 h-full w-full bg-white object-cover"
-                      alt={product.title} // Add alt attribute for accessibility
+                      alt={product.title}
                     />
                   </div>
 
@@ -81,7 +74,8 @@ const Home = ({ searchTerm }) => {
                 </div>
               </Link>
             </div>
-          ))}
+          )
+        )}
       </div>
       <ListPage
         totalPages={Math.ceil(totalItems / itemsPerPage)}
