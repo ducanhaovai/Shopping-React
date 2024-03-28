4fc49c31700d67ba0aeec4ec630081ecaabbbb29
ducanhaovai <ducanhaovai@users.noreply.github.com>
2024-03-28 17:53:14 +0700

    Update UserForm.tsx

--- Shopping-React/frontEnd/src/components/UserDetail/UserForm.tsx
+++ Shopping-React/frontEnd/src/components/UserDetail/UserForm.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-
-import axios from "axios";
 import Input from "../Input";
 import { useForm } from "react-hook-form";
 import { fetchUserProfile, updateUserProfile } from "../../api/profileApi";
