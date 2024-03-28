1a7ef3ec0cd7062fde7e0afcc0c37f2c1706aa6e
ducanhaovai <ducanhaovai@users.noreply.github.com>
2024-03-28 15:51:00 +0700

    test

--- Shopping-React/frontEnd/src/api/productApi.tsx
+++ Shopping-React/frontEnd/src/api/productApi.tsx
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const fetchProducts = async () => {
   try {
-    const response = await axios.get("http://localhost:8088/products");
+    const response = await axios.get(
+      "https://shopping-react-2bwz2hsny-ducanhaovais-projects.vercel.app/products"
+    );
     return response.data;
   } catch (error) {
     console.error("Error fetching products:", error);
